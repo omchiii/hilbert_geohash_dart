adce4f34c5f722bc730aef2a42e8b5250c1da241
Amer <amer_hrnjic@hotmail.com>
2021-03-07 11:45:46 +0100

    Exposing only necessary methods, renaming, default parameters added, precision 10, bitsPerChar 4.

--- lib/hilbert_geohash.dart
+++ lib/hilbert_geohash.dart
@@ -7,7 +7,7 @@ import 'package:tuple/tuple.dart';
 class HilbertGeoHash {
   final Tuple2 latInterval = const Tuple2<double, double>(-90.0, 90.0);
   final Tuple2 lngInterval = const Tuple2<double, double>(-180.0, 180.0);
-  geoHashEncode(double lng, double lat, int precision, int bitsPerChar) {
+  String encode(double lng, double lat, {int precision = 10, int bitsPerChar = 4}) {
     if (lng > lngInterval.item1 &&
         lng < lngInterval.item2 &&
         lat > latInterval.item1 &&
@@ -19,21 +19,22 @@ class HilbertGeoHash {
 
       /// INLINE CONVERT COORDS TO INT
       if (dim >= 1) {
-        var coords = _coord2int(lat, lng, dim);
-        var x = coords.item1;
-        var y = coords.item2;
+        var coords = _coordinatesToInt(lat, lng, dim);
 
-        var code = _xy2hash(x, y, dim);
+
+        var code = _intCoordinatesToHash(coords.item1, coords.item2, dim);
         if (code < 0) {
           throw Error();
         }
-        var codeString = encodeInt(code, bitsPerChar);
+        var codeString = _encodeInt(code, bitsPerChar);
         return codeString;
       }
+    } else {
+      throw ArgumentError('Invalid arguments, check if lat/long are within earths bounds or if percision is positive.');
     }
   }
 
-  Tuple2<int, int> _coord2int(lat, lng, dim) {
+  Tuple2<int, int> _coordinatesToInt(lat, lng, dim) {
     assert(dim >= 1);
     var latY = (lat + latInterval.item2) / 180.0 * dim;
     var lngX = (lng + lngInterval.item2) / 360.0 * dim;
@@ -43,7 +44,7 @@ class HilbertGeoHash {
         min(dim - 1, int.parse(latY.floor().toInt().toString())));
   }
 
-  int _xy2hash(x, y, dim) {
+  int _intCoordinatesToHash(x, y, dim) {
     var d = 0;
     var lvl = dim >> 1;
     while (lvl > 0) {
@@ -52,7 +53,7 @@ class HilbertGeoHash {
 
       d += lvl * lvl * ((3 * rx) ^ ry);
 
-      var xy = _rotate(lvl, x, y, rx, ry);
+      var xy = _rotateXY(lvl, x, y, rx, ry);
       x = xy.item1;
       y = xy.item2;
       lvl >>= 1;
@@ -60,7 +61,7 @@ class HilbertGeoHash {
     return d;
   }
 
-  Tuple2<int, int> _rotate(lvl, x, y, rx, ry) {
+  Tuple2<int, int> _rotateXY(lvl, x, y, rx, ry) {
     if (ry == 0 && rx == 1) {
       var tempy = y;
       y = lvl - 1 - x;
@@ -73,7 +74,7 @@ class HilbertGeoHash {
     return Tuple2(x, y);
   }
 
-  range(int stop, {int start: 0, int step: 1}) {
+  _range(int stop, {int start = 0, int step = 1}) {
     if (step == 0) throw Exception("Step cannot be 0");
 
     return start < stop == step > 0
@@ -82,14 +83,13 @@ class HilbertGeoHash {
         : [];
   }
 
-  encodeInt(code, bitsPerChar) {
+  _encodeInt(code, bitsPerChar) {
     if (code < 0) {
       throw ArgumentError('The hashcode cannot be negative.');
     }
     if (bitsPerChar == 4) {
       var encoded = code.toRadixString(16);
       var encodedSubstring = encoded;
-      print(encoded);
       if (encoded.toString().endsWith('L')) {
         var encodedSubstring = encoded.toString();
         encodedSubstring =
@@ -97,7 +97,6 @@ class HilbertGeoHash {
       } else {
         encodedSubstring = String.fromCharCodes(utf8.encode(encoded));
       }
-      print(encodedSubstring);
       return encodedSubstring;
     }
     if (bitsPerChar == 2) {
@@ -105,8 +104,7 @@ class HilbertGeoHash {
       var codeLen = ((code.bitLength + 1) / 2).floor();
       var res = ['0'];
       var elements = res.map((e) => e * codeLen).toList();
-      for (var i in range(codeLen - 1, start: -1, step: -1)) {
-        // var temp = code & bi(0b11);
+      for (var i in _range(codeLen - 1, start: -1, step: -1)) {
         elements[i] = _base4[code & 3];
         code >>= 2;
       }
@@ -114,18 +112,18 @@ class HilbertGeoHash {
     }
   }
 
-  Tuple2<double, double> decode(code, bitsPerChar) {
+  Tuple2<double, double> decode(code, {int bitsPerChar = 4}) {
     if (code.length == 0) {
       return Tuple2(0.0, 0.0);
     }
 
-    var decodedExactly = decodeExactly(code, bitsPerChar);
+    var decodedExactly = decodeExactly(code, bitsPerChar: bitsPerChar);
 
     return Tuple2(decodedExactly.item1, decodedExactly.item2);
   }
 
   Map<String, String> neighbours(code, bitsPerChar) {
-    var decodedExactly = decodeExactly(code, bitsPerChar);
+    var decodedExactly = decodeExactly(code, bitsPerChar: bitsPerChar);
     var precision = code.length;
     var lng = decodedExactly.item1;
     var lat = decodedExactly.item2;
@@ -145,8 +143,8 @@ class HilbertGeoHash {
     }
 
     Map neighbursDict = {
-      'east': geoHashEncode(east, lat, precision, bitsPerChar),
-      'west': geoHashEncode(west, lat, precision, bitsPerChar),
+      'east': encode(east, lat, precision: precision, bitsPerChar: bitsPerChar),
+      'west': encode(west, lat, precision: precision, bitsPerChar: bitsPerChar),
       'north': '',
       'north-east': '',
       'north-west': '',
@@ -157,27 +155,26 @@ class HilbertGeoHash {
 
     if (north <= 90) {
       neighbursDict.update('north',
-          (value) => geoHashEncode(lng, north, precision, bitsPerChar));
-      // neighburs_dict.addEntries
+          (value) => encode(lng, north, precision: precision, bitsPerChar: bitsPerChar));
       neighbursDict.update('north-east',
-          (value) => geoHashEncode(east, north, precision, bitsPerChar));
+          (value) => encode(east, north, precision: precision, bitsPerChar: bitsPerChar));
       neighbursDict.update('north-west',
-          (value) => geoHashEncode(west, north, precision, bitsPerChar));
+          (value) => encode(west, north, precision: precision, bitsPerChar: bitsPerChar));
     }
 
     if (south >= -90) {
       neighbursDict.update('south',
-          (value) => geoHashEncode(lng, south, precision, bitsPerChar));
+          (value) => encode(lng, south, precision: precision, bitsPerChar: bitsPerChar));
       neighbursDict.update('south-east',
-          (value) => geoHashEncode(east, south, precision, bitsPerChar));
+          (value) => encode(east, south, precision: precision, bitsPerChar: bitsPerChar));
       neighbursDict.update('south-west',
-          (value) => geoHashEncode(west, south, precision, bitsPerChar));
+          (value) => encode(west, south, precision: precision, bitsPerChar: bitsPerChar));
     }
 
     return neighbursDict;
   }
 
-  Tuple4 decodeExactly(code, bitsPerChar) {
+  Tuple4 decodeExactly(code, {bitsPerChar = 4}) {
     if (bitsPerChar != 2 && bitsPerChar != 4 && bitsPerChar != 6) {
       throw ArgumentError('The base is not supported.');
     }
@@ -188,12 +185,12 @@ class HilbertGeoHash {
     var level = bits >> 1;
     var dim = 1 << level;
 
-    var codeInt = decodeInt(code, bitsPerChar);
-    var xy = _hash2xy(codeInt, dim);
+    var codeInt = _decodeInt(code, bitsPerChar);
+    var xy = _hashToCoordinates(codeInt, dim);
     var x = xy.item1;
     var y = xy.item2;
-    var lnglat = _int2coord(x, y, dim);
-    var lngerrlaterr = lvlError(level);
+    var lnglat = _intToCoordinates(x, y, dim);
+    var lngerrlaterr = _lvlError(level);
 
     return Tuple4(
         lnglat.item1 + lngerrlaterr.item1,
@@ -202,12 +199,12 @@ class HilbertGeoHash {
         lngerrlaterr.item2);
   }
 
-  Tuple2<double, double> lvlError(lvl) {
+  Tuple2<double, double> _lvlError(lvl) {
     var error = 1 / (1 << lvl);
     return Tuple2(180 * error, 90 * error);
   }
 
-  Tuple2<double, double> _int2coord(x, y, dim) {
+  Tuple2<double, double> _intToCoordinates(x, y, dim) {
     assert(dim >= 1);
     assert(x < dim);
     assert(y < dim);
@@ -217,7 +214,7 @@ class HilbertGeoHash {
     return Tuple2(lng, lat);
   }
 
-  Tuple2<int, int> _hash2xy(code, dim) {
+  Tuple2<int, int> _hashToCoordinates(code, dim) {
     assert(code <= dim * (dim - 1));
     var x = 0;
     var y = 0;
@@ -225,7 +222,7 @@ class HilbertGeoHash {
     while (lvl < dim) {
       var rx = 1 & (code >> 1);
       var ry = 1 & (code ^ rx);
-      var xy = _rotate(lvl, x, y, rx, ry);
+      var xy = _rotateXY(lvl, x, y, rx, ry);
       x = xy.item1;
       y = xy.item2;
       x += lvl * rx;
@@ -236,33 +233,33 @@ class HilbertGeoHash {
     return Tuple2(x, y);
   }
 
-  decodeInt(tag, bitsPerChar) {
+  _decodeInt(tag, bitsPerChar) {
     if (bitsPerChar == 6) {
-      return decodeInt64(tag);
+      return _decodeInt64(tag);
     }
     if (bitsPerChar == 4) {
-      return decodeInt16(tag);
+      return _decodeInt16(tag);
     }
     if (bitsPerChar == 2) {
-      return decodeInt4(tag);
+      return _decodeInt4(tag);
     }
   }
 
-  decodeInt64(t) {
+  _decodeInt64(t) {
     if (t.length == 0) {
       return 0;
     }
     return int.parse(t, radix: 64);
   }
 
-  decodeInt16(t) {
+  _decodeInt16(t) {
     if (t.length == 0) {
       return 0;
     }
     return int.parse(t, radix: 16);
   }
 
-  decodeInt4(t) {
+  _decodeInt4(t) {
     if (t.length == 0) {
       return 0;
     }
